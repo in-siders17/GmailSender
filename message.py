6d1617b9e178f6598923cd741d562f8bc97c3e37
Soyb1 <sasha.adamow05@gmail.com>
2023-02-17 22:41:13 +0200

    Add parsing

--- message.py
+++ message.py
@@ -1,4 +1,5 @@
 import smtplib
+import argparse
 
 def send_email(sender, password, receiver, message):
     server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -8,18 +9,18 @@ def send_email(sender, password, receiver, message):
         server.login(sender, password)
         server.sendmail(sender, receiver, message)
 
-        return "The message was sent succesfuly!"
+        return "The message was sent successfully!"
     except Exception as _ex:
         return f"{_ex}\nCheck your login or password please!"
 
-
 def main():
-    sender = "boyhich3@gmail.com"
-    password = "tmtzabdqcjmmrpwu"
-    receiver = "shtrikker28@gmail.com"
-    message = input("Type your message: ")
-    print(send_email(sender, password, receiver, message))
-
+    parser = argparse.ArgumentParser(description='Sending email')
+    parser.add_argument('-s', dest="sender", help='sender')
+    parser.add_argument('-p', dest="password", help='password')
+    parser.add_argument('-r', dest="receiver", help='receiver')
+    parser.add_argument('-m', dest="message", help='message')
+    args = parser.parse_args()
+    send_email(sender=args.sender, password=args.password, receiver=args.receiver, message=args.message)
 
 if __name__ == "__main__":
     main()
