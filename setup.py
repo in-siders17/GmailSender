78d9a86389cca0192e15bb0d0362de583e2727e0
Soyb1 <sasha.adamow05@gmail.com>
2023-02-27 20:08:34 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='GmailSender',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='in_siders17',
+    author_email='sasha.adamow05@gmail.com',
+    description='Send message for gmail'
+)
